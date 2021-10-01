6f62a5b88e335b9098436a89a7c55b31ddcc3f69
munroe1786 <era3munroe@gmail.com>
2021-09-30 21:19:56 -0500

    delete previous code from photo container

--- nasagram/src/PhotoContainer.js
+++ nasagram/src/PhotoContainer.js
@@ -7,24 +7,12 @@ const PhotoContainer = (props) => {
         <div>
             {props.photos.map(photo =>
                 <li key={photo.id}>
-                    <Photo url={photo.url} />
+                    <Photo url={photo.url} 
+                            title={photo.title}    
+                    />
                 </li>)}
         </div>
     )
-    
-    //const displayPhotos = () => {
-        //{props.photos.map(photo => 
-          //  <li key={photo.id}>
-            //return <Photo url={photo.url} />;
-            //</li>
-        //);
-    //};
-
-    //return (
-      ////  <>
-          //  <section>{displayPhotos()}</section>
-        //</>
-    //);
 };
 
 export default PhotoContainer;
